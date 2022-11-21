acf1de7dd3f51ac4e7366d35756e4962eba9a88a
stepan103ua <stepan8877ua@gmail.com>
2022-11-21 11:25:52 +0200

    feat: add endpoints keys

---
+++ lib/src/core/constants/endpoints_keys.dart
@@ -0,0 +1,8 @@
+class EndpointsKeys {
+  static const baseUrl =
+      "http://sbbankingapp-env.eba-3teik5g7.eu-central-1.elasticbeanstalk.com";
+
+  static const loginEndpoint = "/login";
+
+  static const registerEndpoint = "/register";
+}
