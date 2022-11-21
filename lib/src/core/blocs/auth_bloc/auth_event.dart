31f9c607ba65be0014de38f7c29b631cb515094c
stepan103ua <stepan8877ua@gmail.com>
2022-11-21 11:16:19 +0200

    feat: add auth bloc

---
+++ lib/src/core/blocs/auth_bloc/auth_event.dart
@@ -0,0 +1,18 @@
+part of 'auth_bloc.dart';
+
+abstract class AuthEvent extends Equatable {
+  const AuthEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AuthLogoutEvent extends AuthEvent {}
+
+class AuthCheckStatusEvent extends AuthEvent {}
+
+class AuthRegisterEvent extends AuthEvent {
+  final RegisterModel registerModel;
+
+  const AuthRegisterEvent(this.registerModel);
+}
