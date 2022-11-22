282e334a7503dfb2085073e6cca086e1c98c47e1
stepan103ua <stepan8877ua@gmail.com>
2022-11-22 15:04:50 +0200

    refactor: change base url

--- lib/src/core/constants/endpoints_keys.dart
+++ lib/src/core/constants/endpoints_keys.dart
@@ -1,6 +1,5 @@
 class EndpointsKeys {
-  static const baseUrl =
-      "http://sbbankingapp-env.eba-3teik5g7.eu-central-1.elasticbeanstalk.com";
+  static const baseUrl = "http://10.0.2.2:7070";
 
   static const loginEndpoint = "/login";
 
