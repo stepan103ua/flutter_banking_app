08cf044fdf8833d4f396409e027e360519edacbe
stepan103ua <stepan8877ua@gmail.com>
2022-11-22 15:09:34 +0200

    refactor: remove unnecessary imports

--- lib/src/features/auth/presentation/widgets/login_tab_view/login_mode_selector.dart
+++ lib/src/features/auth/presentation/widgets/login_tab_view/login_mode_selector.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_banking/src/features/auth/presentation/bloc/cubit/login_mode_cubit.dart';
 
+import '../../bloc/login_mode_cubit/login_mode_cubit.dart';
 import 'login_mode_selector_item.dart';
 
 class LoginModeSelector extends StatelessWidget {
