129b9b71156f28d3f6d00830cc32e0001dcff853
stepan103ua <stepan8877ua@gmail.com>
2022-11-22 15:05:15 +0200

    feat: add validation

--- lib/src/core/constants/validation.dart
+++ lib/src/core/constants/validation.dart
@@ -1,6 +1,6 @@
 class AppValidation {
   static String? passwordValidation(String? value) {
-    if (value == null) {
+    if (value == null || value.isEmpty) {
       return 'Field can not be empty';
     }
     if (!AppValidationRegex.passwordRegex.hasMatch(value)) {
@@ -11,8 +11,7 @@ class AppValidation {
   }
 
   static String? phoneNumberValidation(String? value) {
-    print('Phone validation');
-    if (value == null) {
+    if (value == null || value.isEmpty) {
       return 'Field can not be empty';
     }
     if (!AppValidationRegex.phoneNumberRegex.hasMatch(value)) {
@@ -23,7 +22,7 @@ class AppValidation {
   }
 
   static String? credentialsValidation(String? value) {
-    if (value == null) {
+    if (value == null || value.isEmpty) {
       return 'Field can not be empty';
     }
     if (!AppValidationRegex.credentialsRegex.hasMatch(value)) {
@@ -34,13 +33,39 @@ class AppValidation {
   }
 
   static String? passportValidation(String? value) {
-    if (value == null) {
+    if (value == null || value.isEmpty) {
       return 'Field can not be empty';
     }
     if (!AppValidationRegex.oldPassportFormat.hasMatch(value) &&
         !AppValidationRegex.sixteenPassportFormat.hasMatch(value) &&
         !AppValidationRegex.twentyPassportFormat.hasMatch(value)) {
       return 'Invalid passport data';
+    }
+
+    return null;
+  }
+
+  static String? confirmPasswordValidation(String? value, String? password) {
+    final passwordValidation = AppValidation.passwordValidation(value);
+
+    if (passwordValidation != null) {
+      return passwordValidation;
+    }
+
+    if (value != password) {
+      return 'Passwords do not match';
+    }
+
+    return null;
+  }
+
+  static String? ipnValidation(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Field can not be empty';
+    }
+
+    if (!AppValidationRegex.ipnRegex.hasMatch(value)) {
+      return 'Invalid IPN';
     }
 
     return null;
@@ -53,10 +78,11 @@ class AppValidationRegex {
   static const _phoneNumberRegex =
       r"^\+?([0-9]{2})\)?[-. ]?([0-9]{5})[-. ]?([0-9]{5})$";
   static const _credentialsRegex =
-      r"/^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/";
+      r"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$";
   static const _oldPassportFormat = r"^[А-ЯA-Z]{2}\d{6}$";
   static const _sixteenPassportFormat = r"^\d{13}$";
   static const _twentyPassportFormat = r"^\d{9}$";
+  static const _ipnRegex = r'^\d{10}$';
 
   static RegExp get passwordRegex => RegExp(_passwordRegex);
   static RegExp get phoneNumberRegex => RegExp(_phoneNumberRegex);
@@ -64,4 +90,5 @@ class AppValidationRegex {
   static RegExp get oldPassportFormat => RegExp(_oldPassportFormat);
   static RegExp get sixteenPassportFormat => RegExp(_sixteenPassportFormat);
   static RegExp get twentyPassportFormat => RegExp(_twentyPassportFormat);
+  static RegExp get ipnRegex => RegExp(_ipnRegex);
 }
