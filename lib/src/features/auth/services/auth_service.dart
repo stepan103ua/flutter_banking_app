83f21fd31d57c167c3d70a1d62e8e51de41e50a7
stepan103ua <stepan8877ua@gmail.com>
2022-11-28 22:15:53 +0200

    feat: add refresh token and check pincode

--- lib/src/features/auth/services/auth_service.dart
+++ lib/src/features/auth/services/auth_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_banking/src/core/data/session_data_provider.dart';
 import 'package:flutter_banking/src/core/exceptions/auth_response_error_exception.dart';
+import 'package:flutter_banking/src/core/exceptions/no_internet_connection_exception.dart';
 import 'package:flutter_banking/src/features/auth/data/auth_api_provider.dart';
 import 'package:flutter_banking/src/features/auth/models/login_model.dart';
 import 'package:flutter_banking/src/features/auth/models/register_model.dart';
@@ -45,6 +46,24 @@ class AuthService {
     }
   }
 
+  Future<String?> refreshToken() async {
+    try {
+      final refreshToken = await _sessionDataProvider.getRefreshToken;
+      if (refreshToken == null) {
+        return 'No refresh token';
+      }
+      final responseData = await _authApiProvider.refreshToken(refreshToken);
+
+      await _saveToSession(responseData);
+
+      return null;
+    } on AuthResponseErrorException catch (error) {
+      return error.message;
+    } on NoInternetConnectionException catch (error) {
+      return error.message;
+    }
+  }
+
   Future<void> _saveToSession(Map<String, dynamic> data) async {
     await _sessionDataProvider.saveAccessToken(data['access_token']);
     await _sessionDataProvider
@@ -53,4 +72,7 @@ class AuthService {
     await _sessionDataProvider
         .saveRefreshTokenExpireDate(data['refresh_expire_date']);
   }
+
+  Future<bool> checkPincode(String pincode) async =>
+      (await _sessionDataProvider.pincode) == pincode;
 }
