286a709039e82001fb301bb21b3c23ca6351d0b3
stepan103ua <stepan8877ua@gmail.com>
2022-11-30 13:12:02 +0200

    feat: add card provider string translator

--- lib/src/core/enums/card_providers.dart
+++ lib/src/core/enums/card_providers.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_banking/src/core/abstract_classes/translator.dart';
 
+import '../errors/exceptions/unknown_enum_value_exception.dart';
+
 enum CardProviders { mastercard, visa, unknown }
 
 class CardProviderTranslator extends Translator<CardProviders> {
@@ -15,6 +17,24 @@ class CardProviderTranslator extends Translator<CardProviders> {
         return CardProviders.mastercard;
       default:
         return CardProviders.unknown;
+    }
+  }
+}
+
+class CardProvidersStringTranslator extends Translator<String> {
+  final CardProviders _cardProviders;
+
+  CardProvidersStringTranslator(this._cardProviders);
+
+  @override
+  String get value {
+    switch (_cardProviders) {
+      case CardProviders.mastercard:
+        return 'Mastercard';
+      case CardProviders.visa:
+        return 'Visa';
+      case CardProviders.unknown:
+        throw UnknownEnumValueException('Unknown card provider');
     }
   }
 }
