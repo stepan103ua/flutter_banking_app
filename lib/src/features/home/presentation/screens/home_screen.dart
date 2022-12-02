3b95a6b2a46a3feb15a501a0e856c0a4ebe254ac
stepan103ua <stepan8877ua@gmail.com>
2022-12-02 18:38:20 +0200

    refactor: add card carousel

--- lib/src/features/home/presentation/screens/home_screen.dart
+++ lib/src/features/home/presentation/screens/home_screen.dart
@@ -1,19 +1,26 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_banking/src/core/data/session_data_provider.dart';
+import 'package:flutter_banking/src/features/home/presentation/widgets/card_carousel/card_carousel.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  double _getCarouselHeight(double width) => width * 0.83 * 0.6;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () async {
-              await SessionDataProvider().logout();
-              Navigator.of(context).pushReplacementNamed('/');
-            },
-            child: const Text('Log Out')),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) => Column(
+            children: [
+              const Text('Home'),
+              CardCarousel(
+                height: _getCarouselHeight(constraints.maxWidth),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
