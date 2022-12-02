4502dc213c0799c70495843f14d834ce4377001e
stepan103ua <stepan8877ua@gmail.com>
2022-12-02 18:37:45 +0200

    feat: add card carousel widget

---
+++ lib/src/features/home/presentation/widgets/card_carousel/card_carousel.dart
@@ -0,0 +1,38 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_banking/src/core/enums/card_providers.dart';
+import 'package:flutter_banking/src/core/enums/card_types.dart';
+import 'package:flutter_banking/src/features/home/presentation/widgets/bank_card/bank_card.dart';
+
+class CardCarousel extends StatelessWidget {
+  CardCarousel({super.key, required this.height});
+  final double height;
+  final cards = [
+    BankCard(
+      cardNumber: '1234567890123456',
+      expirationDate: DateTime.now(),
+      cardProvider: CardProviders.visa,
+      cardType: CardTypes.credit,
+    ),
+    BankCard(
+      cardNumber: '1234567890123456',
+      expirationDate: DateTime.now(),
+      cardProvider: CardProviders.mastercard,
+      cardType: CardTypes.debit,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider.builder(
+      itemCount: cards.length,
+      itemBuilder: (context, index, realIndex) => cards[index],
+      options: CarouselOptions(
+        aspectRatio: 0.6,
+        viewportFraction: 0.83,
+        enlargeCenterPage: true,
+        enableInfiniteScroll: false,
+        height: height,
+      ),
+    );
+  }
+}
