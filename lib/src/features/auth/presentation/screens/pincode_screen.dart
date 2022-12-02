cfb6f3507e8d5c44a42c5d400fc71654ebb96656
stepan103ua <stepan8877ua@gmail.com>
2022-12-02 18:36:51 +0200

    feat: add navigation to auth

--- lib/src/features/auth/presentation/screens/pincode_screen.dart
+++ lib/src/features/auth/presentation/screens/pincode_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_banking/src/core/config/routes.dart';
 import 'package:flutter_banking/src/features/auth/presentation/bloc/pincode_cubit/pincode_cubit.dart';
 import 'package:flutter_banking/src/features/auth/presentation/bloc/pincode_screen_bloc/pincode_screen_bloc.dart';
 import 'package:flutter_banking/src/features/auth/presentation/widgets/pincode/pincode_header.dart';
@@ -30,47 +31,53 @@ class PincodeScreen extends StatelessWidget {
             return SafeArea(
               child: isLoading(state)
                   ? const PincodeLoading()
-                  : LayoutBuilder(builder: (context, constraints) {
-                      return SizedBox(
-                        height: constraints.maxHeight,
-                        width: constraints.maxWidth,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            PincodeHeader(height: constraints.maxHeight * 0.1),
-                            Container(
-                              alignment: Alignment.center,
-                              height: constraints.maxHeight * 0.1,
-                              child: const PincodeOutput(),
-                            ),
-                            SizedBox(
-                              height: constraints.maxHeight * 0.55,
-                              child: FittedBox(
-                                child: PincodeKeyboard(
-                                  width: constraints.maxWidth,
-                                  height: constraints.maxHeight,
-                                ),
+                  : LayoutBuilder(
+                      builder: (context, constraints) {
+                        return SizedBox(
+                          height: constraints.maxHeight,
+                          width: constraints.maxWidth,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              PincodeHeader(
+                                  height: constraints.maxHeight * 0.1),
+                              Container(
+                                alignment: Alignment.center,
+                                height: constraints.maxHeight * 0.1,
+                                child: const PincodeOutput(),
                               ),
-                            ),
-                            SizedBox(
-                              height: constraints.maxHeight * 0.07,
-                            ),
-                            SizedBox(
-                              height: constraints.maxHeight * 0.1,
-                              child: FittedBox(
-                                child: TextButton(
-                                  onPressed: () => {},
-                                  child: const Text(
-                                    'Login with password',
+                              SizedBox(
+                                height: constraints.maxHeight * 0.55,
+                                child: FittedBox(
+                                  child: PincodeKeyboard(
+                                    width: constraints.maxWidth,
+                                    height: constraints.maxHeight,
                                   ),
                                 ),
                               ),
-                            )
-                          ],
-                        ),
-                      );
-                    }),
+                              SizedBox(
+                                height: constraints.maxHeight * 0.07,
+                              ),
+                              SizedBox(
+                                height: constraints.maxHeight * 0.1,
+                                child: FittedBox(
+                                  child: TextButton(
+                                    onPressed: () => {
+                                      Navigator.of(context)
+                                          .pushReplacementNamed(AppRoutes.auth)
+                                    },
+                                    child: const Text(
+                                      'Login with password',
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
             );
           },
         ),
