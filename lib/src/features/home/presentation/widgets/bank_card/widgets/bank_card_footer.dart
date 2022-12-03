4606f606914b3ecc6d11c850c6a953eca1e3e2f9
stepan103ua <stepan8877ua@gmail.com>
2022-12-04 00:14:55 +0200

    refactor: refactor height scale

--- lib/src/features/home/presentation/widgets/bank_card/widgets/bank_card_footer.dart
+++ lib/src/features/home/presentation/widgets/bank_card/widgets/bank_card_footer.dart
@@ -6,13 +6,27 @@ import '../bank_card_constants.dart';
 
 class BankCardFooter extends StatelessWidget {
   const BankCardFooter(
-      {super.key, required this.expirationDate, required this.cardProvider});
+      {super.key,
+      required this.expirationDate,
+      required this.cardProvider,
+      required this.height});
 
   final DateTime expirationDate;
   final CardProviders cardProvider;
+  final double height;
 
   String expirationDateText(DateTime expirationDate) =>
       'Exp ${DateFormat('M/yy').format(expirationDate)}';
+
+  Widget _buildFittedWidget(double height, Widget child) {
+    return SizedBox(
+      height: height,
+      child: FittedBox(
+        fit: BoxFit.scaleDown,
+        child: child,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +34,17 @@ class BankCardFooter extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(expirationDateText(expirationDate), style: bankCardTextStyle),
-        Icon(
-          providerIcons[cardProvider],
-          size: 40,
-          color: Colors.white,
+        _buildFittedWidget(
+          height,
+          Text(expirationDateText(expirationDate), style: bankCardTextStyle),
+        ),
+        _buildFittedWidget(
+          height,
+          Icon(
+            providerIcons[cardProvider],
+            size: 40,
+            color: Colors.white,
+          ),
         ),
       ],
     );
