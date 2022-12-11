4a85434cffcc96e790bac11da926b2006f7ebddd
stepan103ua <stepan8877ua@gmail.com>
2022-12-11 14:26:15 +0200

    refactor: refactor home screen

--- lib/src/features/home/presentation/screens/home_screen.dart
+++ lib/src/features/home/presentation/screens/home_screen.dart
@@ -1,65 +1,56 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_banking/src/features/home/presentation/widgets/card_carousel/card_carousel.dart';
-import 'package:flutter_banking/src/features/home/presentation/widgets/frequently_used_functions/frequently_used_functions.dart';
+import 'package:flutter_banking/src/core/constants/constants.dart';
+import 'package:flutter_banking/src/features/home/presentation/bloc/cubit/home_bottom_app_bar_cubit.dart';
+import 'package:flutter_banking/src/features/home/presentation/bloc/home_screen_bloc/home_screen_bloc.dart';
+import 'package:flutter_banking/src/features/home/presentation/widgets/home_screen/home_app_bar.dart';
+import 'package:flutter_banking/src/features/home/presentation/widgets/home_screen/home_floating_action_button.dart';
+import 'package:flutter_banking/src/features/home/presentation/widgets/home_screen/navigation_tabs/home_tab.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../widgets/home_screen/home_bottom_navigation_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  double _getCarouselHeight(double width) => width * 0.82 * 0.6;
+  static const _screens = {
+    HomeScreenAppBarState.home: HomeTab(),
+    HomeScreenAppBarState.expenses: HomeTab(),
+    HomeScreenAppBarState.cards: HomeTab(),
+    HomeScreenAppBarState.profile: HomeTab(),
+  };
 
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     final width = mediaQuery.size.width;
-    final screenHeight = mediaQuery.size.height - mediaQuery.padding.top - 60;
     final horizontalPadding = width * 0.09;
     final appBarHorizontalPadding = horizontalPadding - 12;
-    final heightWithoutCardCarousel = screenHeight - _getCarouselHeight(width);
-    final frequentlyUsedFunctionsHeight =
-        max(heightWithoutCardCarousel * 0.3, 150).toDouble();
-    final spacingHeight = heightWithoutCardCarousel * 0.05;
-
-    return Scaffold(
-      appBar: AppBar(
-        leading: Builder(builder: (context) {
-          return Transform.translate(
-            offset: Offset(appBarHorizontalPadding, 0),
-            child: IconButton(
-              onPressed: Scaffold.of(context).openDrawer,
-              icon: const Icon(Icons.menu_rounded),
-              splashRadius: 24,
-            ),
-          );
-        }),
-        actions: [
-          Transform.translate(
-            offset: Offset(-1 * appBarHorizontalPadding, 0),
-            child: IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.notifications_rounded),
-              splashRadius: 24,
-            ),
-          ),
-        ],
-      ),
-      drawer: const Drawer(),
-      body: SafeArea(
-        child: Column(
-          children: [
-            CardCarousel(
-              height: _getCarouselHeight(width),
-            ),
-            SizedBox(height: spacingHeight),
-            FrequentlyUsedFunctions(
-              height: frequentlyUsedFunctionsHeight,
-              padding: EdgeInsets.symmetric(
-                horizontal: horizontalPadding,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => HomeBottomAppBarCubit(),
+        ),
+        BlocProvider(
+          create: (context) => HomeScreenBloc()..add(HomeScreenLoadEvent()),
+        ),
+      ],
+      child: BlocBuilder<HomeBottomAppBarCubit, HomeScreenAppBarState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: PreferredSize(
+              preferredSize: const Size.fromHeight(kAppBarHeight),
+              child: HomeAppBar(
+                horizontalPadding: appBarHorizontalPadding,
               ),
             ),
-          ],
-        ),
+            drawer: const Drawer(),
+            body: _screens[state],
+            bottomNavigationBar: HomeBottomNavigationBar(state: state),
+            floatingActionButton: const HomeFloatingActionButton(),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+          );
+        },
       ),
     );
   }
